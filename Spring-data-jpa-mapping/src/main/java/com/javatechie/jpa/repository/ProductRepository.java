f4adaf5f5ba69908648bf05a746c0d119b003dd5
moggana1808 <moggana18@gmail.com>
2025-08-16 19:03:45 +0530

    Project

--- Spring-data-jpa-mapping/src/main/java/com/javatechie/jpa/repository/ProductRepository.java
+++ Spring-data-jpa-mapping/src/main/java/com/javatechie/jpa/repository/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.javatechie.jpa.repository;
 
-public class ProductRepository {
+import com.javatechie.jpa.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
 
+public interface ProductRepository extends JpaRepository<Product,Integer> {
 }
