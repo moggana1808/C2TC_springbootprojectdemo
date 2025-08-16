1b126477ebc0cb20506e29ebc1112c808bc52065
moggana1808 <moggana18@gmail.com>
2025-08-16 19:06:22 +0530

    Project

---
+++ OneToOneMapping/src/test/java/com/tns/onetoone/controller/OneToOneMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.onetoone.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneToOneMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
