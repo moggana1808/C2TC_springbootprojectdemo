55ac6f0f504ec7d7890a5b3fc67a0e46fbfd4cfa
moggana1808 <moggana18@gmail.com>
2025-08-16 19:04:47 +0530

    Project

---
+++ Manytomanymapping/src/main/java/com/tns/many_to_many_mapping/controller/ManytomanymappingApplication.java
@@ -0,0 +1,13 @@
+package com.tns.many_to_many_mapping.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManytomanymappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManytomanymappingApplication.class, args);
+	}
+
+}
---
+++ Manytomanymapping/src/test/java/com/tns/course/controller/ManytomanymappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tns.course.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManytomanymappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
